038f81873dd3df887fd259a312aba6904423cb4f
Victor Wybenga <wybengavi055@hwcdsb.ca>
2026-04-29 13:21:00 -0400

    its brokem

--- Scripts/elevator.gd
+++ Scripts/elevator.gd
@@ -1,10 +1,4 @@
 extends StaticBody2D
-
-const SPEED: float = 100.0
-const FLOORS: int = 10
-
-var target_floors: Array[int] = []
-
 enum ElevatorState {
 	IDLE,
 	MOVING_UP,
@@ -13,25 +7,58 @@ enum ElevatorState {
 	DOOR_CLOSING
 }
 
+const SPEED: float = 100.0
+const FLOORS: int = 10
+
+var current_floor: int
+var target_floor: int
+var called_floors: Array[bool] = []
+var state: ElevatorState
+
 signal reached_floor(floor: int)
+
+func get_floor(y_position: float) -> int:
+	var height: float = get_viewport_rect().size.y
+	var bound_height: float = $CollisionShape2D.get_shape().get_rect().size.y
+	var usable_height: float = height - bound_height
+	return round(FLOORS - (FLOORS / usable_height) * (y_position - (3 * bound_height / 4)))
 
 func get_floor_position(floor: int) -> float:
 	var height: float = get_viewport_rect().size.y
 	var bound_height: float = $CollisionShape2D.get_shape().get_rect().size.y
-	var usable: float = height - bound_height
-	return (usable / FLOORS) * (10 - floor) + (3 * bound_height / 4)
+	var usable_height: float = height - bound_height
+	return (usable_height / FLOORS) * (FLOORS - floor) + (3 * bound_height / 4)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	state = ElevatorState.IDLE
+	called_floors.resize(10)
+	called_floors.fill(false)
+	
+func next_floor(current_floor: int) -> void:
+	var found_above = called_floors.slice(current_floor).find(true) + 1
+	var found_below = called_floors.slice(0, current_floor).find(true) + 1
+	
+	if found_above:
+		if state == ElevatorState.MOVING_UP:
+			target_floor = found_above
+		else:
+			state = ElevatorState.MOVING_DOWN
+	elif found_below:
+		if state == ElevatorState.MOVING_DOWN:
+			target_floor = found_below
+		else:
+			state = ElevatorState.MOVING_UP
+	else:
+		state = ElevatorState.IDLE
+	
 
 func move_to_floor(delta: float):
-	if not target_floors:
+	if state == ElevatorState.IDLE:
 		return
 	
 	var target = Vector2(
 		self.position.x, 
-		get_floor_position(target_floors[0])
+		get_floor_position(target_floor)
 	)
 	if position.distance_to(target) > SPEED * delta:
 		move_and_collide(
@@ -39,14 +66,23 @@ func move_to_floor(delta: float):
 		)
 	else:
 		position = target
-		var floor = target_floors.pop_front()
-		reached_floor.emit(floor)
+		state = ElevatorState.IDLE
+		called_floors[target_floor - 1] = false
+		reached_floor.emit(target_floor)
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	current_floor = get_floor(self.position.y)
+	next_floor(current_floor)
 	move_to_floor(delta)
 
 
 func _on_elevator_buttons_call_elevator(floor: int) -> void:
-	target_floors.push_back(floor)
+	if floor > current_floor:
+		state = ElevatorState.MOVING_UP
+	elif floor < current_floor:
+		state = ElevatorState.MOVING_DOWN
+	else:
+		state = ElevatorState.IDLE
+		
+	called_floors[floor - 1] = true
--- Scripts/queue_display.gd
+++ Scripts/queue_display.gd
@@ -2,4 +2,8 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.text = ("Queue: %s") % (" -> ".join($"../Elevator".target_floors))
+	self.text = ("Queue: %s\nCurrent Floor: %d\nCurent State: %s") % [
+		" -> ".join($"../Elevator".called_floors), 
+		$"../Elevator".current_floor,
+		$"../Elevator".state
+	]
